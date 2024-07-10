f420029090223fabf208731acdb550f1c789573c
unknown <chamidukeshikaz@gmail.com>
2024-07-11 00:15:25 +0530

    day 01

---
+++ store/src/test/java/com/spring_project/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_project.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
